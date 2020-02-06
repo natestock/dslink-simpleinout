86406d3e2a760c6b7c4c75f2fcc0f39f4cecd391
natestock <nathanestock@gmail.com>
2020-02-06 11:11:34 -0500

    body to json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const {DSLink, RootNode, BaseLocalNode} = require("dslink");
+const {DSLink, RootNode, BaseLocalNode, ValueNode} = require("dslink");
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8444;
 
@@ -7,17 +8,41 @@ class Board extends BaseLocalNode {
   constructor(path, provider) {
     super(path, provider);
   }
+  loadChild(name, data) {
+    if (!this.children.has(name)) {
+      if (data['$is'] === Person.profileName) {
+        let node = this.createChild(name, Person);
+        node.load(data);
+        let value = data['?value'];
+        node.setValue(value);
+      }
+    }
+  }
 }
 
-function main() {
+Board.profileName = 'board';
+Board.saveNodeOnChange = true;
+
+class Person extends ValueNode {
+  constructor(path, provider) {
+    super(path, provider);
+  }
+}
+
+Person.profileName = 'person';
+Person.saveNodeOnChange = true;
+
+async function main() {
   let rootNode = new RootNode();
   rootNode.createChild('Board', Board);
-  let link = new DSLink('simpleinout', {rootNode});
-  link.connect();
+  let link = new DSLink('simpleinout', {rootNode, saveNodes: true});
+  await link.connect();
 }
+app.use(bodyParser.json());
 
 app.post('/simpleinout', (req, res, next) => {
-  console.log(req);
+  console.log(req.body);
+  res.status(201).send();
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
